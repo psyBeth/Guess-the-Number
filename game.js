0ba6fed7abf53c4b969cd6b87e33f816134aac2f
Betul <betulkoru06@gmail.com>
2023-12-11 17:55:00 +0300

    starting to JS

---
+++ game.js
@@ -0,0 +1,9 @@
+//? make the computer pick a number
+let randomno= Math.ceil(Math.random()*20)
+console.log(randomno);
+
+let message = document.querySelector(".msg")
+
+let skor = 10;
+// could get the skore from html but we will use it a lot so...
+let highestSkor = 0;
