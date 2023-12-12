a90ae1baeeb60e4489cca173f332ba032f8595b0
Betul <betulkoru06@gmail.com>
2023-12-12 21:02:27 +0300

    JS check btn 1

--- game.js
+++ game.js
@@ -7,3 +7,38 @@ let message = document.querySelector(".msg")
 let skor = 10;
 // could get the skore from html but we will use it a lot so...
 let highestSkor = 0;
+
+//? everytime the check button is clicked these will happen:
+document.querySelector(".check").addEventListener("click", () => {
+
+    const guessin = document.querySelector(".guess").value
+    
+    //! when button is pressed without entering a guess
+    if (!guessin){
+        message.textContent = "Please make a guess first."
+    //! if the guess is right
+    } else if (guessin == randomno){
+        message.textContent = "You're right. Congratulations.👏"
+        document.querySelector("body").style.backgroundColor = "green"
+        document.querySelector(".number").textContent = randomno
+
+        //top score control will come here
+    } else {
+    //! if it's wrong
+        
+        //? score>1 means user can still make a guess
+        if (skor > 1){
+            skor -- ;
+            document.querySelector(".score").textContent = skor
+            
+
+
+        } else{
+            message.textContent = "You lost the game.💀"
+            document.querySelector(".score").textContent = 0;
+            document.querySelector("body").style.backgroundColor = "red"
+        }
+
+    }
+
+})
